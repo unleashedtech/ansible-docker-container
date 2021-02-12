89ce3e08b4976c8e65b63fb0d4cb73d0cef7c524
Colin O'Dell <codell@unleashed-technologies.com>
2021-02-12 10:55:34 -0500

    Build ansible 2.10

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 docker build --build-arg ALPINE_VERSION=3.9 -t unleashed/ansible:2.7 .
 docker build --build-arg ALPINE_VERSION=3.12 -t unleashed/ansible:2.9 .
+docker build --build-arg ALPINE_VERSION=3.13 -t unleashed/ansible:2.10 .
 docker build -t unleashed/ansible:latest .
 
 docker push unleashed/ansible:2.7
 docker push unleashed/ansible:2.9
+docker push unleashed/ansible:2.10
 docker push unleashed/ansible:latest
